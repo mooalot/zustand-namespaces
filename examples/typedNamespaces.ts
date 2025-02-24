89fea7662a870ee062e6a7975040f0e6a99a78da
Gabriel Saunders <igabe@icloud.com>
2025-02-23 23:43:00 -0700

    update examples

--- examples/typedNamespaces.ts
+++ examples/typedNamespaces.ts
@@ -1,6 +1,10 @@
 import { create } from 'zustand';
-import { createNamespace, getNamespaceHooks, namespaced } from '../src/utils';
-import { ExtractNamespaces } from '../src/types';
+import {
+  ExtractNamespaces,
+  createNamespace,
+  getNamespaceHooks,
+  namespaced,
+} from 'zustand-namespaces';
 
 type Namespace1 = {
   dataInNamespace1: string;
